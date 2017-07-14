4e49183657c382c0899e86719f2a0c23293b66d9
LissySangeeta <lissyarvind@gmail.com>
2017-07-14 13:10:47 +0530

    first commit

---
+++ pipetest/src/main/java/pipetest/minimalsanity.java
@@ -0,0 +1,33 @@
+package pipetest;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import junit.framework.Assert;
+
+public class minimalsanity {
+	private WebDriver driver;
+	
+	@BeforeClass
+	public void init()
+	{
+		driver = new FirefoxDriver();
+		driver.get("www.google.com");
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testgooglename()
+	{
+		try {
+			driver.wait(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertEquals("www.google.com", driver.getCurrentUrl());
+	}
+
+}
